83bb79fb939128845db561096a76347e390bcd38
BlushyTears <m123.123.123@hotmail.com>
2026-03-23 00:17:00 +0100

    Add helper function and some more preparatory stuff

--- BaseTile.cs
+++ BaseTile.cs
@@ -5,14 +5,12 @@ using System.Collections.Generic;
 public partial class BaseTile : TextureRect
 {
    [Export] private Godot.Collections.Array<Texture2D> textures = new Godot.Collections.Array<Texture2D>();
-    
     private Vector2 tilesize;
 
     public void SetTexture(int idx)
     {
         Texture = textures[idx];
         tilesize = Texture.GetSize();
-        GD.Print($"Tile size: {tilesize}");
     }
 
     public void SetPosition(Vector2 pos)
--- PCG_Manager.cs
+++ PCG_Manager.cs
@@ -11,7 +11,18 @@ public partial class PCG_Manager : Node2D
     public override void _Ready()
     {
         createTile(0, new Vector2(0, 0));
+        createTile(0, new Vector2(1, 0));
+        createTile(0, new Vector2(0, 1));
         createTile(1, new Vector2(1, 1));
+
+        var v = GetTile(new Vector2(0, 0));
+        GD.Print("v after: ", v);
+    }
+
+    public BaseTile GetTile(Vector2 pos)
+    {
+        int idx = (int)pos.Y * tileSize + (int)pos.X;    
+        return tileset[idx];
     }
 
     private void createTile(int textureIdx, Vector2 pos)
@@ -24,5 +35,7 @@ public partial class PCG_Manager : Node2D
         tile.SetPosition(pos);
 
         tileset.Add(tile);
+
+        GD.Print("v before: ", tile);
     }
 }
