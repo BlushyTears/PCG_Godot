a4ee1d5781be2c1ce149458dc4648847cc305fa1
BlushyTears <m123.123.123@hotmail.com>
2026-03-24 11:48:03 +0100

    Make some basic automata thing that only does first 2 columns for some reason

--- BaseTile.cs
+++ BaseTile.cs
@@ -6,12 +6,14 @@ public partial class BaseTile : TextureRect
 {
    [Export] private Godot.Collections.Array<Texture2D> textures = new Godot.Collections.Array<Texture2D>();
     private Vector2 tilesize;
+    public int textureIdx = 0;
 
     public void SetTexture(int idx)
     {
         ZIndex = 0;
         Texture = textures[idx];
         tilesize = Texture.GetSize();
+        textureIdx = idx;
     }
 
     public void SetPosition(Vector2 pos)
--- PCG_Manager.cs
+++ PCG_Manager.cs
@@ -8,12 +8,14 @@ public partial class PCG_Manager : Node2D
     [Export] private PackedScene baseTileScene;
     private List<BaseTile> tileset = new List<BaseTile>();
     private int tileSize;
+    private const int horizontalTileCount = 18;
+    private const int vertocalTileCount = 10;
     public override void _Ready()
     {
         Random random = new Random();
-        for(int i = 0; i < 18; i++)
+        for(int i = 0; i < horizontalTileCount; i++)
         {
-            for(int j = 0; j < 10; j++)
+            for(int j = 0; j < vertocalTileCount; j++)
             {
                 int rnd = random.Next(0, 2);
                 createTile(rnd, new Vector2(i, j));
@@ -41,7 +43,33 @@ public partial class PCG_Manager : Node2D
 
     void Generate()
     {
-        GD.Print("Generate btn");        
+        for(int i = 0; i < horizontalTileCount; i++)
+        {
+            int neighboorCount = 0;
+            for(int j = 0; j < vertocalTileCount; j++)
+            {
+                BaseTile tile = GetTile(new Vector2(i, j));
+
+                // only check for horizontal and vertical neighboors
+                for(int i_prime = -1; i_prime <= 1; i_prime++)
+                {
+                    for(int j_prime = -1; j_prime <= 1; j_prime++)
+                    {
+                        BaseTile compareTile = GetTile(new Vector2(i, j));
+
+                        if(tile.textureIdx == compareTile.textureIdx)
+                        {   
+                            neighboorCount++;
+                        }
+                    }    
+                }
+
+                if(neighboorCount > 2)
+                {
+                    tile.SetTexture(0);                   
+                }
+            }
+        }    
     }
 
     void Clear()
